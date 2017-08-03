8b2f1a0bfed57c494eeabec7c56654da16a867cd
Dugal Harris <dugalh@users.noreply.github.com>
2017-08-04 00:30:03 +0200

    convert sageoid2010 to gtx (geotransform needs checking)

---
+++ Code/Results/Quickbird Orthorectification/CreateGcpFileForBasic1B.py
@@ -0,0 +1,204 @@
+
+import gdal
+import ogr
+import osr
+
+
+# For basic level 1b image
+# This makes a text file for using in PCI that has the Geo co-ords and image locs of my GCP's
+# Note that I (stupidly) first collected the image locs in a projected co-ord system, so this needs
+# to be converted back to image pixel locs
+
+# file containing actual GCP locs in WGS84
+gcpGeoLocFile = "C:/Data/Development/Projects/PhD GeoInformatics/Docs/Misc/Baviaanskloof/BaviiaansPeCorrectedGcpMay2017Combined.shp"
+
+# file containing image locations of GCP locs in UTM 35S
+gcpImLocFile = "C:/Data/Development/Projects/PhD GeoInformatics/Docs/Misc/Baviaanskloof/Quickbird-056844553010_01-Gcp.shp"
+
+# imFile = "D:/Data/Development/Projects/PhD GeoInformatics/Data/Digital Globe/056549293010_01/PanSharpen/03NOV18082012-P2AS_R1C12-056549293010_01_P001_GdalPanSharp.tif"
+# imFile = "D:/Data/Development/Projects/PhD GeoInformatics/Data/Digital Globe/056549293010_01/056549293010_01_P001_PAN/03NOV18082012-P2AS_R1C1-056549293010_01_P001.TIF"
+# imFile = "D:/Data/Development/Projects/PhD GeoInformatics/Data/Digital Globe/056549293010_01/056549293010_01_P001_PAN/Assembled/03NOV18082012-P2AS_R1C12-056549293010_01_P001.TIF"
+# nb the im is in wgs84
+imFile = "D:/Data/Development/Projects/PhD GeoInformatics/Data/Digital Globe/056844553010_01/056844553010_01_P001_PAN/03NOV18082012-P1BS-056844553010_01_P001.TIF"
+
+
+# read in Geo CoOrds of GCP's (and height!)
+ds = ogr.Open(gcpGeoLocFile)
+if ds is None:
+    print "Open failed./n"
+
+lyr = ds.GetLayerByIndex(0)
+lyr.ResetReading()
+spatialRef = lyr.GetSpatialRef()
+
+
+# gcpList = []
+gcpDict = {}
+for (i, feat) in enumerate(lyr):
+    if i > 190:
+        break
+    print '.',
+    feat_defn = lyr.GetLayerDefn()
+    f = {}
+    for i in range(feat_defn.GetFieldCount()):
+        field_defn = feat_defn.GetFieldDefn(i)
+        f[field_defn.GetName()] = feat.GetField(i)
+    geom = feat.GetGeometryRef()
+    if geom is not None and (geom.GetGeometryType() == ogr.wkbPoint or geom.GetGeometryType() == ogr.wkbPoint25D):
+        print "%s %.6f, %.6f" % (f['Comment'], geom.GetX(), geom.GetY())
+        f['geom'] = geom
+        f['X'] = geom.GetX()
+        f['Y'] = geom.GetY()
+        f['Z'] = f['GNSS_Heigh']   #? - should be able to get this from geom
+    else:
+        print "no point geometry/n"
+    # gcpList.append(f)
+    gcpDict[f['Comment']] = f
+print ' '
+ds = None
+
+
+# read in UTM CoOrds of GCP's image locs
+ds = ogr.Open(gcpImLocFile)
+if ds is None:
+    print "Open failed./n"
+
+lyr = ds.GetLayerByIndex(0)
+lyr.ResetReading()
+spatialRefImLoc = lyr.GetSpatialRef()
+
+imGcpList = []
+imGcpDict = {}
+
+for (i, feat) in enumerate(lyr):
+    if i > 190:
+        break
+    print '.',
+    feat_defn = lyr.GetLayerDefn()
+    f = {}
+    for i in range(feat_defn.GetFieldCount()):
+        field_defn = feat_defn.GetFieldDefn(i)
+        f[field_defn.GetName()] = feat.GetField(i)
+    geom = feat.GetGeometryRef()
+    if geom is not None and (geom.GetGeometryType() == ogr.wkbPoint or geom.GetGeometryType() == ogr.wkbPoint25D):
+        print "%s %.6f, %.6f" % (f['Comment'], geom.GetX(), geom.GetY())
+        f['geom'] = geom
+        f['X'] = geom.GetX()
+        f['Y'] = geom.GetY()
+        f['Xi'] = 0   # im locs
+        f['Yi'] = 0
+    else:
+        print "no point geometry/n"
+    # imGcpList.append(f)
+    imGcpDict[f['Comment']] = f
+print ' '
+ds = None
+
+# open image to get extents in proj co-ords
+# read in Geo CoOrds of GCP's (and height!)
+ds = gdal.Open(imFile)
+if ds is None:
+    print "Open failed./n"
+
+
+print 'Driver: ', ds.GetDriver().ShortName,'/', \
+      ds.GetDriver().LongName
+print 'Size is ',ds.RasterXSize,'x',ds.RasterYSize, \
+      'x',ds.RasterCount
+print 'Projection is ',ds.GetProjection()
+geotransform = ds.GetGeoTransform()
+if not geotransform is None:
+    print 'Origin = (',geotransform[0], ',',geotransform[3],')'
+    print 'Pixel Size = (',geotransform[1], ',',geotransform[5],')'
+
+geotransform = [0., 0., 0., 0., 0., 0.]
+if True:  # hack for projectionless image
+    geotransform[0] = 24.30712463
+    geotransform[3] = -33.59966141
+    geotransform[1] = (24.50332750 - 24.30712463) / ds.RasterXSize
+    geotransform[5] = (-33.59966141 - -33.74584942) / ds.RasterYSize
+# 24.3071246305866104,-33.7458494236889948 : 24.5033274972737871,-33.5996614073278437
+# extents from qgis
+# u'24.30712463 -33.74584942, 24.30712463 -33.59966141, 24.50332750 -33.59966141, 24.50332750 -33.74584942, 24.30712463 -33.74584942'
+
+def world2Pixel(geoMatrix, x, y):
+  """
+  Uses a gdal geomatrix (gdal.GetGeoTransform()) to calculate
+  the pixel location of a geospatial coordinate
+  """
+  ulX = geoMatrix[0]
+  ulY = geoMatrix[3]
+  xDist = geoMatrix[1]
+  yDist = geoMatrix[5]
+  rtnX = geoMatrix[2]
+  rtnY = geoMatrix[4]
+  pixel = ((x - ulX) / xDist)
+  line = abs((y - ulY) / yDist)
+  return (pixel, line)
+
+# xform = osr.CoordinateTransformation(spatialRefImLoc, osr.SpatialReference(ds.GetProjectionRef()))
+
+for f in imGcpDict.values():
+    (xi, yi) = world2Pixel(geotransform, f['X'], f['Y'])
+
+    f['Xi'] = xi
+    f['Yi'] = yi
+    print "%s %i, %i" % (f['Comment'], xi, yi)
+    if xi < 0 or xi > ds.RasterXSize:
+        print '-------------------------------xi out of bounds'
+    if yi < 0 or yi > ds.RasterYSize:
+        print '--------------------------------yi out of bounds'
+
+
+# Create text file
+
+# Character descriptions:
+# I: the GCP's identification number.
+#
+# X: the geocoded X coordinate.
+#
+# Y: the geocoded Y coordinate.
+#
+# P: the pixel location of the GCP on the uncorrected image file.
+#
+# L: the line location of the GCP on the uncorrected image file.
+#
+# E: the elevation of the GCP.
+#
+# D: data to be ignored.
+#
+# For example, the string IXYE represents the layout where I is the GCP's identification, X and Y are the GCP's geocoded x and y coordinates, and E is the GCP's elevation.
+#
+# The line in the text file and the Format must match. Lines that do not match the Format are ignored.
+
+
+#pci format
+gcpFile = open("C:/Data/Development/Projects/PhD GeoInformatics/Docs/Misc/Baviaanskloof/Quickbird-056844553010-Gcp.txt", 'w')
+gcpFile.write("I\tP\tL\tX\tY\tE\n")
+id = 1
+for key in imGcpDict:
+    # key = f['Comment']
+    fgcp = gcpDict[key]
+    fim = imGcpDict[key]
+    #print "%s %.6f, %.6f" % (f['Comment'], geom.GetX(), geom.GetY())
+    gcpFile.write("%s\t%.3f\t%.3f\t%.9f\t%.9f\t%.2f\n" % (key, fim['Xi'], fim['Yi'], fgcp['X'], fgcp['Y'], fgcp['Z']))
+    id = id + 1
+    print "%s" % (key)
+gcpFile.close()
+
+
+#arcmap format
+transform = osr.CoordinateTransformation(spatialRef, spatialRefIm)
+gcpFile = open("C:/Data/Development/Projects/PhD GeoInformatics/Docs/Misc/Baviaanskloof/Quickbird-056844553010-Gcp-Arcmap.txt", 'w')
+for key in imGcpDict:
+    # key = f['Comment']
+    fgcp = gcpDict[key]
+    fim = imGcpDict[key]
+    # print "%s %.6f, %.6f" % (f['Comment'], geom.GetX(), geom.GetY())
+    point = ogr.Geometry(ogr.wkbPoint)
+    point.AddPoint(fgcp['X'], fgcp['Y'])
+    point.Transform(transform)
+    gcpFile.write("%.3f %.3f %.3f %.3f\n" % (fim['X'], fim['Y'], point.GetX(), point.GetY()))
+    id = id + 1
+    print "%s" % (key)
+gcpFile.close()
---
+++ Code/Results/Quickbird Orthorectification/SAGeoidToGtx.py
@@ -0,0 +1,88 @@
+#!/usr/bin/env python
+#******************************************************************************
+# 
+#  Project:  Vertical Datum
+#  Purpose:  Translate EGM2008 binary file into gtx format.
+#  Author:   Frank Warmerdam, warmerdam@pobox.com
+# 
+#******************************************************************************
+#  Copyright (c) 2010, Frank Warmerdam
+# 
+#  Permission is hereby granted, free of charge, to any person obtaining a
+#  copy of this software and associated documentation files (the "Software"),
+#  to deal in the Software without restriction, including without limitation
+#  the rights to use, copy, modify, merge, publish, distribute, sublicense,
+#  and/or sell copies of the Software, and to permit persons to whom the
+#  Software is furnished to do so, subject to the following conditions:
+# 
+#  The above copyright notice and this permission notice shall be included
+#  in all copies or substantial portions of the Software.
+# 
+#  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+#  OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+#  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+#  THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+#  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+#  FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+#  DEALINGS IN THE SOFTWARE.
+#******************************************************************************
+
+import sys
+import string
+from osgeo import osr
+from osgeo import gdal
+import numpy as np
+import pylab
+
+# =============================================================================
+
+# my adaptation to make a gtx file for sa geoid
+# if __name__ == '__main__':
+if True:
+    # argv = gdal.GeneralCmdLineProcessor( sys.argv )
+    # if argv is None:
+    #     sys.exit( 0 )
+    #
+    # if len(argv) != 1:
+    #     Usage()
+
+    geoidFile = "D:/Data/Development/Projects/PhD GeoInformatics/Data/NGI/SA Geoid/SAGEOID2010.dat"
+    dat = np.loadtxt(geoidFile, dtype=np.float32) #, delimiter = ',')
+    ncols = 409
+    nrows = 128017/ncols
+    heights = np.reshape(dat[:, 2], (nrows, ncols))
+    pylab.imshow(heights)
+    lat_delta = np.median(np.diff(dat[:ncols-1, 1]))
+    long_delta = np.median(np.diff(dat[::ncols, 0]))
+
+#     vrt = """<VRTDataset rasterXSize="8640" rasterYSize="4321">
+#   <VRTRasterBand dataType="Float32" band="1" subClass="VRTRawRasterBand">
+#     <SourceFilename>Und_min2.5x2.5_egm2008_isw=82_WGS84_TideFree</SourceFilename>
+#     <ImageOffset>4</ImageOffset>
+#     <PixelOffset>4</PixelOffset>
+#     <LineOffset>34568</LineOffset>
+#     <ByteOrder>MSB</ByteOrder>
+#   </VRTRasterBand>
+# </VRTDataset>"""
+#
+#     ds_vrt = gdal.Open(vrt)
+#     egm_m = ds_vrt.GetRasterBand(1).ReadAsArray()
+#     ds_vrt = None
+
+    drv = gdal.GetDriverByName('GTX')
+    ds_gtx = drv.Create('D:/Data/Development/Projects/PhD GeoInformatics/Data/NGI/SA Geoid/sageoid2010_25.gtx',
+                        ncols, nrows, 1, gdal.GDT_Float32 )
+    ps_25 = 2.5 / 60.0
+    # ds_gtx.SetGeoTransform( (-180 - ps_25,ps_25,0,90 + ps_25,0,-1 * ps_25) )
+
+    # the below needs checking/fixing
+    ds_gtx.SetGeoTransform((np.min(dat[:,1]) - lat_delta, lat_delta, 0, np.min(dat[:, 0]) - long_delta, 0, 1 * long_delta))
+
+    # east = egm_m[:,:4320] * 1.0
+    # west = egm_m[:,4320:] * 1.0
+
+    ds_gtx.GetRasterBand(1).WriteArray(heights) #( east, 4320, 0 )
+    #ds_gtx.GetRasterBand(1).WriteArray( west, 0, 0 )
+    
+    ds_gtx = None
+    
