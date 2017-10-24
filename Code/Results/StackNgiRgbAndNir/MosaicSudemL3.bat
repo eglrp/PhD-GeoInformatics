7eaf0b1a2b825c7d3529ed5d7f11e645d221de7e
Dugal Harris <dugalh@users.noreply.github.com>
2017-10-24 09:42:37 +0200

    minor shuffling

--- Code/Results/StackNgiRgbAndNir/MosaicSudemL3.bat
+++ Code/Results/StackNgiRgbAndNir/MosaicSudemL3.bat
@@ -11,7 +11,7 @@ cd D:\Data\Development\Projects\PhD GeoInformatics\Data\CGA\SUDEM L3 Unedited
 D:
 
 gdal_merge.py -o x3323da-b_2015_L3a.tif -n -150 -co "COMPRESS=LZW" -co "BIGTIFF=YES" -a_nodata -150 x3323da_2015_L3a.tif x3323db_2015_L3a.tif
-gdaladdo --config COMPRESS_OVERVIEW LZW –config BIGTIFF_OVERVIEW=YES -ro -r average x3323da-b_2015_L3a.tif 2 4 8 16 32
+gdaladdo -ro --config COMPRESS_OVERVIEW LZW –-config BIGTIFF_OVERVIEW=YES -r average x3323da-b_2015_L3a.tif 2 4 8 16 32
 
 gdal_merge.py -o x3324c_2015_L3a.tif -n -150 -co "COMPRESS=DEFLATE" -co "BIGTIFF=YES" -a_nodata -150 x3324ca_2015_L3a.tif x3324cb_2015_L3a.tif x3324cd_2015_L3a.tif
-gdaladdo --config COMPRESS_OVERVIEW LZW –config BIGTIFF_OVERVIEW=YES -ro -r average x3324c_2015_L3a.tif 2 4 8 16 32
+gdaladdo -ro --config COMPRESS_OVERVIEW LZW –-config BIGTIFF_OVERVIEW=YES -r average x3324c_2015_L3a.tif 2 4 8 16 32
