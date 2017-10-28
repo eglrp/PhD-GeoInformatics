9d143cc460ac0867a053d927a309b62a4d13098b
Dugal Harris <dugalh@users.noreply.github.com>
2017-10-28 11:11:48 +0200

    fixing up path issues with VS studio 2017, GDAL and OpenCV: 1) Put opencv and gdal at the beginning of the path otherwise we get some error about not finding ms vc runtime dlls i.e. the path ordering is NB 2) Use EQUAL and not _stricmp as recommended due to ???

--- Code/Results/NGI Calibration/PostProcess.bat
+++ Code/Results/NGI Calibration/PostProcess.bat
@@ -1,12 +1,23 @@
-echo on
-REM CMD /V:ON /C
-setlocal EnableDelayedExpansion
+echo off
+SET OSGEO4W_ROOT=C:\OSGeo4W64\
+REM SET OTB=%OSGEO4W_ROOT%apps\orfeotoolbox
 
-for %%i in (*_RGBN_DS_2_5.tif) do (
+CALL %OSGEO4W_ROOT%bin\o4w_env.bat
+
+setlocal EnableDelayedExpansion
+REM echo on
+REM CMD /V:ON /C
+E:
+cd "E:\Data\NGI\Rectified\3324C_2015_1004\RGBN\XCALIB"
+
+for %%i in (o*_RGBN_XCALIB.tif) do (
 set jj=%%i
 REM  -co "TILED=YES" -co "BLOCKXSIZE=512" -co "BLOCKYSIZE=512" 
 REM !jj:~1!
-gdal_translate.exe -mo "BitsPerSample=12" -a_nodata 0 %%i ./PostProc/%%i
+REM gdal_translate.exe -mo "BitsPerSample=12" -a_nodata 0 %%i ./PostProc/%%i
+echo %%i
+gdaladdo -clean %%i
+gdaladdo -ro -r average --config COMPRESS_OVERVIEW PACKBITS %%i 2 4 8 16 32
 )
 
 pause
