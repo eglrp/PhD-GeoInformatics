366fb800b3fce39ca669174208ca43368bd436a8
Dugal Harris <dugalh@users.noreply.github.com>
2017-10-30 17:02:09 +0200

    ...

--- Code/Results/Baviaans2017FieldTrialAnalysis/DeadwoodCsAnalysis.py
+++ Code/Results/Baviaans2017FieldTrialAnalysis/DeadwoodCsAnalysis.py
@@ -8,17 +8,19 @@ from scipy.stats import gaussian_kde
 import collections
 
 
-
-def EvalRecordCs(record):
-    # dist in m !!
+def EvalRecordCs(record, plotArea=(10. ** 2) / (100. ** 2)  ):   # plotArea in ha
+    # formula from CDM doc: A/R Methodological Tool - Eq 6
+    # equation uses various units!!
     # thumb suck params
-    cfTree = 0.6
-    Dj = 0.6
-    L = 100   # length of transects
+
+    cfTree = 0.48  # carbon fraction tree biomass
+    Dj = 0.5   # basic wood density (guess from a quick look at literature) (t d.m/m^3)
+                # Note that basic density is similar to the wet/dry ration in Marius' table
+    L = 100.   # length of transects (m)
     densityReduction = [1., 0.8, 0.45]
-    # formula from CDM doc: A/R Methodological Tool
-    yc = (10.**2) * (44./12) * cfTree * Dj * (np.pi*np.pi/(8*L)) * ((record['diameter']/1000.)**2) * densityReduction[record['densityState']-1]
+    yc = plotArea * (44./12) * cfTree * Dj * ((np.pi**2)/(8.*L)) * ((record['diameter']/10.)**2) * densityReduction[record['densityState']-1]
     return yc
+
 
 def EvalPlotCs(allometricModels, plot):
     plotYc = []
@@ -59,7 +61,12 @@ for sheetName in sheetNames:
                 record['densityState'] = 1L
             elif type(record['densityState']) is not long:
                 print 'WARNING: unknown density state: ', record['densityState']
-            record['yc'] = EvalRecordCs(record)
+
+            if plotName.startswith('INT'):
+                record['yc'] = EvalRecordCs(record, plotArea=(10. ** 2) / (100. ** 2))
+            else:
+                record['yc'] = EvalRecordCs(record, plotArea=(10. ** 2) / (100. ** 2))
+
             plot.append(record)
             print '.',
         if plots.__contains__(plotName):
@@ -79,16 +86,16 @@ for plotKey, plot in plots.iteritems():
     height = np.float64([record['diameter'] for record in plot])
 
     kde = gaussian_kde(height)  #, bw_method=bandwidth / height.std(ddof=1))
-    heightGrid = np.linspace(0, 300, 100)
+    heightGrid = np.linspace(0, 600, 100)
     heightKde = kde.evaluate(heightGrid)
     pylab.subplot(2, 3, i)
     pylab.plot(heightGrid, heightKde)
     axLim = pylab.axis()
     # h = pylab.plot([50, 50], [0, heightKde.max()], 'r')
     pylab.grid('on')
-    pylab.xlabel('Plant Diam. (mm)', fontdict={'size':fontSize})
+    pylab.xlabel('Dead Wood Diam. (mm)', fontdict={'size':fontSize})
     pylab.ylabel('Prob.(diam.)', fontdict={'size':fontSize})
-    pylab.title('Diam. distribution for plot %s' % (plotKey), fontdict={'size':fontSize})
+    pylab.title('Diameter distribution for plot %s' % (plotKey), fontdict={'size':fontSize})
     # if i >= plots.__len__():
     #     pylab.legend(h, ['50mm threshold'], loc='upper left', bbox_to_anchor=(1.2, 1), prop={'size':fontSize})
     pylab.axis([axLim[0], axLim[1], 0, heightKde.max()])
@@ -108,10 +115,10 @@ for plotKey, plot in plots.iteritems():
     # h = pylab.plot([50, 50], [axLim[2], axLim[3]], 'r')
     pylab.axis(axLim)
     pylab.grid('on')
-    pylab.xlabel('Plant Diam. (mm)', fontdict={'size':fontSize})
+    pylab.xlabel('Dead Wood Diam. (mm)', fontdict={'size':fontSize})
     pylab.ylabel('Cum. Distr.(C. stock) (kg)', fontdict={'size':fontSize})
     # pylab.xlim([0, 350])
-    pylab.title('Diam. / C. stock relation for plot %s' % (plotKey), fontdict={'size':fontSize})
+    pylab.title('Diameter / C. stock relation for plot %s' % (plotKey), fontdict={'size':fontSize})
     # if i >= plots.__len__():
     #     pylab.legend(h, ['50cm threshold'], loc='upper left', bbox_to_anchor=(1.2, 1), prop={'size':fontSize})
     i += 1
