3aa568429982a136379af73040f97544fb4c3982
Dugal Harris <dugalh@users.noreply.github.com>
2017-11-24 11:42:43 +0200

    Adding sampling shapefiles so we can track changes

--- Code/Results/NGI Calibration/SUDEManalysis.bat
+++ Code/Results/NGI Calibration/SUDEManalysis.bat
@@ -13,13 +13,13 @@ SET HSFILE="D:\Data\Development\Projects\PhD GeoInformatics\Data\CGA\SUDEM L3 Un
 SET ASPECTFILE="D:\Data\Development\Projects\PhD GeoInformatics\Data\CGA\SUDEM L3 Unedited\x3323db_2015_L3a_GDAL_Aspect.tif"
 SET ROUGHNESSFILE="D:\Data\Development\Projects\PhD GeoInformatics\Data\CGA\SUDEM L3 Unedited\x3323db_2015_L3a_GDAL_Roughness.tif"
 REM Make hillshade with SUN at midday and altitude corresp to Sept 22 - see http://keisan.casio.com/exec/system/1224682277
-echo Hillshade---------------------------------------------------------
+echo Hillshade
 gdaldem hillshade %DEMFILE% %HSFILE% -az 0 -alt 56 -alg Horn -co "COMPRESS=JPEG"
 gdaladdo -ro -r average --config COMPRESS_OVERVIEW JPEG %HSFILE% 2 4 8 16 32
-echo Aspect---------------------------------------------------------
-gdaldem aspect %DEMFILE% %ASPECTFILE% -alg Horn -zero_for_flat -co "COMPRESS=DEFLATE"
-gdaladdo -ro -r average --config COMPRESS_OVERVIEW DEFLATE %ASPECTFILE% 2 4 8 16 32
-echo Roughness---------------------------------------------------------
-gdaldem roughness %DEMFILE% %ROUGHNESSFILE% -co "COMPRESS=DEFLATE"
-gdaladdo -ro -r average --config COMPRESS_OVERVIEW DEFLATE %ASPECTFILE% 2 4 8 16 32
+echo Aspect
+gdaldem aspect %DEMFILE% %ASPECTFILE% -trigonometric -alg Horn -zero_for_flat -co "COMPRESS=LZW"
+gdaladdo -ro -r average --config COMPRESS_OVERVIEW LZW %ASPECTFILE% 2 4 8 16 32
+echo Roughness
+gdaldem roughness %DEMFILE% %ROUGHNESSFILE% -co "COMPRESS=LZW"
+gdaladdo -ro -r average --config COMPRESS_OVERVIEW LZW %ROUGHNESSFILE% 2 4 8 16 32
 pause
