a441b1d50f3e9db577d842bc18329e15e09f68c1
Dugal Harris <dugalh@users.noreply.github.com>
2017-11-27 14:23:09 +0200

    ...

--- Code/Results/NGI Calibration/PostProcess.bat
+++ Code/Results/NGI Calibration/PostProcess.bat
@@ -7,17 +7,19 @@ CALL %OSGEO4W_ROOT%bin\o4w_env.bat
 setlocal EnableDelayedExpansion
 REM echo on
 REM CMD /V:ON /C
-E:
-cd "E:\Data\NGI\Rectified\3324C_2015_1004\RGBN"
+REM E:
+REM cd "E:\Data\NGI\Rectified\3324C_2015_1004\RGBN"
+V:
+cd "V:\Data\NGI\UnRectified\3323D_2015_1001\RGBN"
 
-for %%i in (o*_RGBN.tif) do (
+for %%i in (*_0?_*_RGBN.tif) do (
 set jj=%%i
 REM  -co "TILED=YES" -co "BLOCKXSIZE=512" -co "BLOCKYSIZE=512" 
 REM !jj:~1!
 REM gdal_translate.exe -mo "BitsPerSample=12" -a_nodata 0 %%i ./PostProc/%%i
 echo %%i
-gdaladdo -clean %%i
-gdaladdo -ro -r average --config COMPRESS_OVERVIEW PACKBITS %%i 2 4 8 16 32
+REM gdaladdo -clean %%i
+gdaladdo -ro -r average --config COMPRESS_OVERVIEW JPEG %%i 2 4 8 16 32
 )
 
 pause
